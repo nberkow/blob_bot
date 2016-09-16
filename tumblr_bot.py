b72900a5ab6eec3d0fd5899fef3de94cbee7e4b3
Ubuntu <ubuntu@ip-172-31-18-9.us-west-2.compute.internal>
2016-09-16 19:54:36 +0000

    Updates and cleanup

--- tumblr_bot.py
+++ tumblr_bot.py
@@ -5,13 +5,57 @@ import yaml
 import os
 import urlparse
 import code
-import oauth2 as oauth
+import oauth2
+import subprocess
+import random
+import time 
+import sys
+import re
+import random
 
 if __name__ == "__main__":
 
+	text = " ".join(open(sys.argv[1], 'r').readlines())
+	words = re.split("\s", text)
+	prohibited = ['the', 'MarieFrances', 'mike', 'tablishment']
+	regex = re.compile('[^a-zA-Z]')
+
+	wset = set()
+	for w in words:
+		w = regex.sub('', w)
+		if len(w) > 2 and w not in prohibited:
+			wset.add(w)
+	
+
+
+
+	REQUEST_TOKEN_URL = 'http://www.tumblr.com/oauth/request_token'
+	AUTHORIZATION_URL = 'http://www.tumblr.com/oauth/authorize'
+	ACCESS_TOKEN_URL = 'http://www.tumblr.com/oauth/access_token'
+	CONSUMER_KEY = 'hdXV9JhkLN1tdVivsDFxWozKic48SeKuyZDHQpeGWxN74p0yJV'
+	CONSUMER_SECRET = 'C9iaQjBIasc7HWV2TOXTRpb5sgTkgrhJapifLmxugwzFnOugHS'
+
 	client = pytumblr.TumblrRestClient(
-		'hdXV9JhkLN1tdVivsDFxWozKic48SeKuyZDHQpeGWxN74p0yJV',
-		'C9iaQjBIasc7HWV2TOXTRpb5sgTkgrhJapifLmxugwzFnOugHS',
-		'zgjRhxtLvXvwasWH5u5x1PnuCMsTplzONWhSe4oLn2wfNDBfhU',
-		'1shevDLQ6TvFIDDq7UVpE8VExwnXePW1mZTOCTpbV2wpWl6lhz'
-	)
+	    CONSUMER_KEY,
+	    CONSUMER_SECRET,
+	    "2ySYcIiNvJ8jgYvYiJ7ZSJ17ZekpqZOjqi7xxoRTDaH21RxUmV",
+	    "Ehh8UDqzbNb9XK0FBDsPqvWUu4UBF9mRpFbCcAFyDWcpHroL3y"
+	)	
+
+	def make_and_post():
+		rtags = random.sample(wset, 3)
+		subprocess.call(["Rscript",  "random_plot.R"])
+		client.create_photo('marlykrushkova', state="published", tags=rtags, data="/home/ubuntu/tbot/random_plot.png")
+
+	s = 60 * 60 * 48
+	noise = random.randint(-18,18) * 60 * 60
+
+	while True:
+		print "attempting a post!"
+		make_and_post()
+		time.sleep(s + noise)
+
+
+
+
+
